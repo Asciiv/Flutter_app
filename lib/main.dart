f61170f416f4444f4d3bf0066c60d6da2017b0a7
kartikey <kartikeym1@gmail.com>
2022-08-11 12:14:37 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/Jim.dart';
 
 void main(){
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    //double pi = 3.14;
+   // var day = "tuesday"; //It recogonize any thing string,int
+    //const tpie = 6.28;
+    //final pie = 3.14232;
+    // difference between const and final is that we can change the value of final
+
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+      home: Jim(),
     );
   }
 }
