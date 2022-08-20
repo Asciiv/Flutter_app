fb07995ea671b02fa85cca24c35930f4566b69c9
kartikey <kartikeym1@gmail.com>
2022-08-20 12:18:56 +0530

    Initial Commit

--- lib/pages/jim.dart
+++ lib/pages/jim.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/catalog.dart';
 import 'package:flutter_app/widgets/drawer.dart';
+import 'package:flutter_app/widgets/item_widget.dart';
 
     class Jim extends StatelessWidget {
      final int days = 30;
@@ -11,9 +13,14 @@ import 'package:flutter_app/widgets/drawer.dart';
           appBar: AppBar(
             title: Text("Catalog App"),
           ),
-          body: Center(
-            child: Container(
-            child: Text("Welcome to $days days of Flutter by Kartikey"),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child :ListView.builder(itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index){
+          return ItemWidget(
+            item:CatalogModel.items[index],
+          );
+              },
           ),
           ),
           drawer: MyDrawer(),
